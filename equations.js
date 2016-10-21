684fb9a12b16131893350441275ac5a949412b8c
Jae Lee <ksy903@gmail.com>
2016-10-20 21:30:12 -0500

    alert added to nextPage

--- equations.js
+++ equations.js
@@ -1,4 +1,5 @@
 function toAnotherPage(){
+	alert("Going to page");
 	var options = {
 	  "direction"      : "up", // 'left|right|up|down', default 'right' (Android currently only supports left and right)
 	  "duration"       :  600, // in milliseconds (ms), default 400
